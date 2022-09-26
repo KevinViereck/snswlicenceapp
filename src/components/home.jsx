81d6ad08a1e263e5322e5b5c590c3c7192c5c1c6
DIVYA.ERRA <divya.erra@service.nsw.gov.au>
2022-09-27 09:47:05 +1000

    basic testing

---
+++ src/components/home.jsx
@@ -0,0 +1,33 @@
+import { useState } from "react"
+
+export default function register(){
+
+    const[firstname, setFirstname] = useState("")
+    const[lastname, setLastname] = useState('')
+    const[email,setEmail] = useState('')
+    const[mobileNumber, setMobileNumber] = useState()
+    const[password,setPassword] = useState('')
+
+    return( 
+        <>
+            <h1> Register </h1>
+            <form>
+                <label> First Name: </label>
+                <input type="text" value={firstname} onChange={(e) => setFirstname(e.target.value)} />
+                <br/>
+                <label> Last Name: </label>
+                <input type="text" value={lastname} onChange={(e) => setLastname(e.target.value)} /> 
+                <br/>
+                <label> Email: </label>
+                <input type="text" value={email} onChange={(e) => setEmail(e.target.value)} />
+                <br/>
+                <label> Mobile Number:</label>
+                <input type="text" value={mobileNumber} onChange={(e) => setMobileNumber(e.target.value)} />
+                <br/>
+                <label> Password: </label>
+                <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} />
+                <br/>
+            </form>
+        </>
+    )
+}
