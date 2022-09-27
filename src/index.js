f67a8eb3c16844bfc6e2b3a40bdbef7bb6ec7943
DIVYA.ERRA <divya.erra@service.nsw.gov.au>
2022-09-27 11:48:26 +1000

    add

--- src/index.js
+++ src/index.js
@@ -1,6 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+<<<<<<< HEAD
 import { BrowserRouter } from 'react-router-dom'
+=======
+<<<<<<< HEAD
+<<<<<<< HEAD
+import { BrowserRoute, BrowserRouter } from 'react-router-dom'
+=======
+import { BrowserRouter } from 'react-router-dom'
+>>>>>>> cec900f (corrected spelling browserrouter)
+=======
+import {BrowserRouter } from 'react-router-dom'
+>>>>>>> 274d590 (add)
+>>>>>>> 4d7375c (add)
 import './index.css';
 import App from './App';
 
