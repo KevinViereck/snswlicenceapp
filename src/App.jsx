9f1be0861a30e4cb385100bb19e012f1a97dfbd1
DIVYA.ERRA <divya.erra@service.nsw.gov.au>
2022-09-27 11:23:55 +1000

    updated home

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,15 @@
 import './App.css';
+import {Routes, Route, Link} from 'react-dom'
+import Register from './registration/home'
 
 function App() {
   return (
     <div className="App">
-     <h1 className="text-3xl font-bold underline">Issue Licence</h1>
+      <h1 className="text-3xl font-bold underline">Issue Licence</h1>
+      <Routes>
+        <Route path="/home" element={< Register />} />
+
+      </Routes>
     </div>
   );
 }
