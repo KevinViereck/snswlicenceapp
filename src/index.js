a6c713d46a083eae5d02955de9218e95e01f0d6d
DIVYA.ERRA <divya.erra@service.nsw.gov.au>
2022-09-27 12:03:21 +1000

    fixedmergeissue

--- src/index.js
+++ src/index.js
@@ -1,18 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-<<<<<<< HEAD
-import { BrowserRouter } from 'react-router-dom'
-=======
-<<<<<<< HEAD
-<<<<<<< HEAD
-import { BrowserRoute, BrowserRouter } from 'react-router-dom'
-=======
-import { BrowserRouter } from 'react-router-dom'
->>>>>>> cec900f (corrected spelling browserrouter)
-=======
-import {BrowserRouter } from 'react-router-dom'
->>>>>>> 274d590 (add)
->>>>>>> 4d7375c (add)
+import { BrowserRouter } from 'react-router-dom';
 import './index.css';
 import App from './App';
 
