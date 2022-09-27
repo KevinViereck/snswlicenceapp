9cd4114a77d79e54b7321fcb3037d87d7beee2f9
DIVYA.ERRA <divya.erra@service.nsw.gov.au>
2022-09-27 11:24:51 +1000

    updated index

--- src/index.js
+++ src/index.js
@@ -9,10 +9,8 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <BrowserRoute>
-      <App />
-    
-    </BrowserRoute>
-   
+        <App />
+    </BrowserRoute> 
   </React.StrictMode>
 );
 
