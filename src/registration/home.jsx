753c6d498aa63dd31df1d59e772a88ec9cfa4ee5
DIVYA.ERRA <divya.erra@service.nsw.gov.au>
2022-09-27 14:40:54 +1000

    editedhome

--- src/registration/home.jsx
+++ src/registration/home.jsx
@@ -1,47 +1,58 @@
+
 import { useState } from "react"
+import { useNavigate } from "react-router-dom"
 
 export default function Register(){
 
-    const[firstname, setFirstname] = useState("")
-    const[lastname, setLastname] = useState('')
+    const[firstName, setFirstName] = useState("")
+    const[lastName, setLastName] = useState('')
     const[email,setEmail] = useState('')
-    const[mobileNumber, setMobileNumber] = useState()
+    const[mobile, setMobile] = useState()
     const[password,setPassword] = useState('')
 
-    // function getData(){
+    const navigate = useNavigate()
 
-    //     const myHeaders = new headers()
-    //     myHeaders.append('content-type','application/json')
+    // useEffect(() => {
+    //     getData()
+    // }, []);
 
-    //     const requestOptions ={
-    //         method: 'POST',
-    //         headers : myHeaders,
-    //     }
 
-    //     const response = fetch(`http://localhost:8080`,requestOptions)
-        
-    // }
+   async function getData(){
+        let config = {
+            method: "POST",
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify({ firstName, lastName, email, mobile, password }),
+          };
+          
+        let response = await fetch(`${server}/user`, config);
+
+        let json = response.json()
+            alert("User Registered"); 
+            navigate("/")
+       
+    }    
 
     return( 
         <>
             <h1> Register </h1>
-            <form>
+           <div> 
                 <label> First Name: </label>
-                <input type="text" value={firstname} onChange={(e) => setFirstname(e.target.value)} />
+                <input type="text" value={firstName} onChange={(e) => setFirstName(e.target.value)} />
                 <br/>
                 <label> Last Name: </label>
-                <input type="text" value={lastname} onChange={(e) => setLastname(e.target.value)} /> 
+                <input type="text" value={lastName} onChange={(e) => setLastName(e.target.value)} /> 
                 <br/>
                 <label> Email: </label>
                 <input type="text" value={email} onChange={(e) => setEmail(e.target.value)} />
                 <br/>
                 <label> Mobile Number:</label>
-                <input type="text" value={mobileNumber} onChange={(e) => setMobileNumber(e.target.value)} />
+                <input type="text" value={mobile} onChange={(e) => setMobile(e.target.value)} />
                 <br/>
                 <label> Password: </label>
                 <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} />
                 <br/>
-            </form>
+                <button onClick={getData}> Register </button>
+            </div>
         </>
     )
 }
