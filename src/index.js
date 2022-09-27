47b3c6447c06b0e5e7470af9b1223bd4e9deb475
DIVYA.ERRA <divya.erra@service.nsw.gov.au>
2022-09-27 10:31:23 +1000

    added browserrouter

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import {BrowserRoute} from 'react-router-dom'
 import './index.css';
 import App from './App';
 
@@ -7,12 +8,11 @@ import App from './App';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRoute>
+      <App />
+    
+    </BrowserRoute>
+   
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-
-// THIS IS A TEST COMMENT
