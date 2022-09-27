26caee0c495887d56c64dc8606bbda5b6459bb62
DIVYA.ERRA <divya.erra@service.nsw.gov.au>
2022-09-28 09:40:08 +1000

    added topics

--- src/HomePage.jsx
+++ src/HomePage.jsx
@@ -2,6 +2,8 @@ export default function HomePage(){
     return(
         <>
             <h1> Welcome to the home screen </h1>
+            <p> Logo </p>
+            <p> Register Button </p>
             <p> </p>
         </>
     )
