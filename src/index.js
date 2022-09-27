2078cd1bceead8c9d6ab716bb8316112a9d49504
Kevin Viereck <109639813+KevinViereck@users.noreply.github.com>
2022-09-27 11:50:25 +1000

    working

--- src/index.js
+++ src/index.js
@@ -1,10 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-<<<<<<< HEAD
-import { BrowserRoute, BrowserRouter } from 'react-router-dom'
-=======
 import { BrowserRouter } from 'react-router-dom'
->>>>>>> cec900f (corrected spelling browserrouter)
 import './index.css';
 import App from './App';
 
