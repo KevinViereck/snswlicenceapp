19c1092abead259886f62a706b452bbb6bc8ba4f
DIVYA.ERRA <divya.erra@service.nsw.gov.au>
2022-09-28 22:03:15 +1000

    pages

--- src/HomePage.jsx
+++ src/HomePage.jsx
@@ -1,6 +1,9 @@
 export default function HomePage(){
     return(
         <>
+            <h4 style={top-left}> Home </h4>
+            <p style> Welcome to NSW Driving Licence Application </p>
+
         
         </>
     )
