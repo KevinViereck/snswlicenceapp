928a32da4c42c9049ea9884b77da5d0f1ff3e3ef
DIVYA.ERRA <divya.erra@service.nsw.gov.au>
2022-09-29 03:10:23 +1000

    addingentries

--- src/logbook/PracticeLogEntry.jsx
+++ src/logbook/PracticeLogEntry.jsx
@@ -9,7 +9,23 @@ export default function PracticeLogEntry(){
 
     return(
         <>
-            <h3> Log Hours </h3>
+            <button> Log Hours </button>
+            <table>
+                <thead>
+                    <tr>
+                        <th> Start Time:</th>
+                        <th> End Time: </th>
+                        <th> Total Hours </th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr> 
+                    {
+                        practiceLogEntries.map( (le) => <td> {le.startTime}</td>)
+                    }
+                    </tr>
+                </tbody>
+            </table>
         </>
     )
 }
