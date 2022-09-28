9b1b5a83e5bbb99ec9d1ca079058322c537002d1
DIVYA.ERRA <divya.erra@service.nsw.gov.au>
2022-09-28 16:55:10 +1000

    addedlogin

---
+++ src/logbook/Login.jsx
@@ -0,0 +1,38 @@
+import { useState } from "react"
+
+export default function Login(){
+
+    const[email, setEmail] = useState('')
+    const[password,setPassword] = useState('')
+
+
+    async function getLogin(){
+        
+        const config = {
+            method: "POST",
+            headers : {'Content-type':'application/json'},
+            body: JSON.stringify({email,password})
+        }
+
+        const response = await fetch(`http://localhost:8080/user/login`,config)
+        const result = response.json()
+        alert("logged in as")
+
+    }
+
+    return(
+        <>
+            <div>
+                <label>Email:</label>
+                <input type="text" value={email} onChange={(e) => setEmail(e.target.value)}/>
+            </div>
+            <div>
+                <label>Password</label>
+                <input type="password" value={password} onChange ={(e) => setPassword(e.target.value)} />
+            </div>
+            <div>
+                <button> Login </button>
+            </div>
+        </>
+    )
+}
