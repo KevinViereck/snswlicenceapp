ae9ca7475284e98b49d55fe67cd87ca1952d59a7
DIVYA.ERRA <divya.erra@service.nsw.gov.au>
2022-09-30 16:17:25 +1000

    Co-authored-by: peter_m_holman@hotmail.com <peter_m_holman@hotmail.com>

--- src/registration/Register.jsx
+++ src/registration/Register.jsx
@@ -5,12 +5,12 @@ import nswlogo from "../img/nswlogo.png";
 import { Link } from "react-router-dom";
 
 export default function Register() {
-  const [firstName, setFirstName] = useState("");
-  const [lastName, setLastName] = useState("");
-  const [email, setEmail] = useState("");
-  const [dateOfBirth, setDateOfBirth] = useState("");
-  const [mobile, setMobile] = useState();
-  const [password, setPassword] = useState("");
+  const [firstName, setFirstName] = useState("Tom");
+  const [lastName, setLastName] = useState("Smith");
+  const [email, setEmail] = useState("t@e.com");
+  const [dateOfBirth, setDateOfBirth] = useState("2001-05-14");
+  const [mobile, setMobile] = useState("0123456");
+  const [password, setPassword] = useState("password");
 
   const navigate = useNavigate();
 
@@ -19,15 +19,18 @@ export default function Register() {
   // }, []);
 
   async function getData() {
+
+
     let config = {
       method: "POST",
       headers: { "Content-Type": "application/json" },
-      body: JSON.stringify({ firstName, lastName, email, mobile, password }),
+      body: JSON.stringify( { firstName, lastName, email, dateOfBirth, mobile, password })
     };
 
-    let response = await fetch(`http://localhost:8080/user`, config);
+    let response = await fetch(`http://localhost:8080/user/register`, config);
 
-    let json = response.json();
+    let token = await response.json();
+    debugger
     alert("User Registered");
     navigate("/");
   }
@@ -47,7 +50,7 @@ export default function Register() {
         <br></br>
         <div className="justify-center">
           <div
-            class="inline-flex shadow-md hover:shadow-lg focus:shadow-lg"
+            className="inline-flex shadow-md hover:shadow-lg focus:shadow-lg"
             role="group"
           >
             <div className="navi">
@@ -74,93 +77,90 @@ export default function Register() {
           {/* <p className="text-gray-600 pt-2">Please register here</p> */}
         </section>
 
-        <section className="mt-7">
+        
           <table>
-            <tr>
-              <td width="10px">
-                {" "}
-                <label> First Name </label>
-                <input
-                  className="register-box"
-                  type="text" 
-                  value={firstName}
-                  required="required"
-                  onChange={(e) => setFirstName(e.target.value)}
-                />
-              </td>
-            </tr>
-
-            <tr>
-              <td>
-                {" "}
-                <label> Last Name </label>
-                <input
-                  className="register-box"
-                  type="text"
-                  value={lastName}
-                  required="required"
-                  onChange={(e) => setLastName(e.target.value)}
-                />
-              </td>
-            </tr>
-
-            <tr>
-              <td>
-                {" "}
-                <label> Your Email</label>
-                <input
-                  className="register-box"
-                  type="text"
-                  value={email}
-                  required="required" placeholder="email@email.com"
-                  onChange={(e) => setEmail(e.target.value)}
-                />
-              </td>
-            </tr>
-
-            <tr>
-              <td>
-                {" "}
-                <label> Date of Birth </label>
-                <input
-                  className="register-box"
-                  type="date"
-                  value={dateOfBirth}
-                  required="required"
-                  onChange={(e) => setDateOfBirth(e.target.value)}
-                />
-              </td>
-            </tr>
-
-            <tr>
-              <td>
-                {" "}
-                <label> Mobile Number</label>
-                <input
-                  className="register-box"
-                  type="text"
-                  value={mobile}
-                  required="required" placeholder="+61"
-                  onChange={(e) => setMobile(e.target.value)}
-                />
-              </td>
-            </tr>
-
-            <tr>
-              <td>
-                {" "}
-                <label> Password </label>
-                <input
-                  className="register-box"
-                  type="password"
-                  value={password}
-                  required="required"
-                  onChange={(e) => setPassword(e.target.value)}
-                />
-              </td>
-            </tr>
+            <tbody>
+                <tr>
+                  <td width="10px">
+                    {" "}
+                    <label> First Name </label>
+                    <input
+                      className="register-box"
+                      type="text" 
+                      value={firstName}
+                      required="required"
+                      onChange={(e) => setFirstName(e.target.value)}
+                    />
+                  </td>
+                </tr>
+                <tr>
+                  <td>
+                    {" "}
+                    <label> Last Name </label>
+                    <input
+                      className="register-box"
+                      type="text"
+                      value={lastName}
+                      required="required"
+                      onChange={(e) => setLastName(e.target.value)}
+                    />
+                  </td>
+                </tr>
+                <tr>
+                  <td>
+                    {" "}
+                    <label> Your Email</label>
+                    <input
+                      className="register-box"
+                      type="text"
+                      value={email}
+                      required="required" placeholder="email@email.com"
+                      onChange={(e) => setEmail(e.target.value)}
+                    />
+                  </td>
+                </tr>
+                <tr>
+                  <td>
+                    {" "}
+                    <label> Date of Birth </label>
+                    <input
+                      className="register-box"
+                      type="date"
+                      value={dateOfBirth}
+                      required="required"
+                      onChange={(e) => setDateOfBirth(e.target.value)}
+                    />
+                  </td>
+                </tr>
+                <tr>
+                  <td>
+                    {" "}
+                    <label> Mobile Number</label>
+                    <input
+                      className="register-box"
+                      type="text"
+                      value={mobile}
+                      required="required" placeholder="+61"
+                      onChange={(e) => setMobile(e.target.value)}
+                    />
+                  </td>
+                </tr>
+                <tr>
+                  <td>
+                    {" "}
+                    <label> Password </label>
+                    <input
+                      className="register-box"
+                      type="password"
+                      value={password}
+                      required="required"
+                      onChange={(e) => setPassword(e.target.value)}
+                    />
+                  </td>
+                </tr>
+            </tbody>
           </table>
-        </section>
+      
         <br></br>
         <button className="navi" onClick={getData}> 
           {" "}
