52d9f4522449575ff891697ba1e1d6578b56549c
DIVYA.ERRA <divya.erra@service.nsw.gov.au>
2022-10-04 10:38:05 +1100

    added page code

---
+++ src/logbook/LoginAsCSR.jsx
@@ -0,0 +1,18 @@
+export default function LoginInAsCSR(){
+    return(
+        <>
+            <div>
+                <p> List of Customers </p>
+            </div>
+            <div>
+                <p> link to each customer where their details are only displayed </p>
+            </div>
+            <div>
+                <p> Check the condition whether they are have completed their 120hours and issue licence </p>
+            </div>
+            <div>
+                <p> Issue Licence </p>
+            </div>
+        </>
+    )
+}
