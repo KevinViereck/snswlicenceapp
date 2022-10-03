45718d5cb75bdef47aa2a46ec5f1d7a4613ee589
DIVYA.ERRA <divya.erra@service.nsw.gov.au>
2022-10-04 10:21:56 +1100

    add

---
+++ src/logbook/LoginAsCustomer.jsx
@@ -0,0 +1,38 @@
+import { useState } from "react"
+
+export default function LoginAsInCustomer(){
+
+    const[logentries, setLogentries] = useState('')
+    const[startTime,setStartTime] = useState('')
+    const[finish, setFinish] = useState('')
+    const[day, setDay] = useState('')
+    const[night, setNight] = useState('')
+    const[instructorLed, setInstructorLed] = useState('')
+
+    return(
+        <>
+            <div>
+                <p> Log Book Details </p>
+            </div>
+            <form>
+                <fieldset>
+                    <label> Start Time </label>
+                    <input type="" value={startTime} onChange={(e) => setStartTime(e.target.value)}/>
+                    <div>
+
+                    </div>
+                </fieldset>
+                <fieldset>
+                    <p> Remaining Hours </p>
+                    <p> calculate the hours display those hours</p>
+                </fieldset>
+                <fieldset>
+                    <p> Logged as Customer name to be displayed</p>
+                </fieldset>
+                <button> Add entries </button>
+                <button> Delete entries </button>
+            </form>
+            
+        </>
+    )
+}
