9d7468ddb6c9953172728fe98459c2731cd75ca2
DIVYA.ERRA <divya.erra@service.nsw.gov.au>
2022-10-04 02:53:01 +1100

    ch

--- src/registration/NewUserRegistered.jsx
+++ src/registration/NewUserRegistered.jsx
@@ -1,12 +1,15 @@
 import { useState } from "react"
+import { useParams } from "react-router-dom"
 
 export default function NewUserRegistered(){
 
     const[firstName, setFirstName] = useState('')
+    const {id} = useParams('')
 
     return(
         <>
             <p> enter your log hours </p>
+            
             <p> your log history </p>
         </>
     )
